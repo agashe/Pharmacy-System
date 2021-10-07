daaac7202d49b83eec4fa9790ecf3ae6f68220f0
Mohamed Yousef <engineer.mohamed.yossef@gmail.com>
2021-10-07 10:19:57 +0200

    header complete , working on crud

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,18 +4,18 @@
             <img src="{{ asset('assets/images/pharmacy-logo.png') }}" alt="logo" class="logo">
             {{ __('Pharmacy System') }}
         </a>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-light" type="submit"><i class="fa fa-search"></i></button>
+        <form class="d-flex search-box">
+            <input class="form-control me-2" type="search" placeholder="{{ __('Type something ...') }}" aria-label="Search">
+            <button class="btn btn-light text-primary" type="submit"><i class="fa fa-search"></i></button>
         </form>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 float-right">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-star"></i> Add New 
+                    <i class="fa fa-star"></i> {{ __('Add New') }}
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#">{{ __('Pharmacy') }}</a></li>
